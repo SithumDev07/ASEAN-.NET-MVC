a40512a5f5e1bef47437183db8701c15108a14fe
Supun222 <74459467+Supun222@users.noreply.github.com>
2022-05-12 01:17:14 +0530

    Added warning and mandatory icons

--- Web Handson MVC/Scripts/LearningPlan.js
+++ Web Handson MVC/Scripts/LearningPlan.js
@@ -14,9 +14,9 @@ const dataSet = [
         courseCategory: "Orientation",
         courseStatus: "In Progress",
         lableColor: 'yellow',
-        dueDate: "25-MAR-2022",
-        remainDays: 12,
-        action: "Resume"
+        dueDate: "25-JUN-2022",
+        action: "Resume",
+        mandatory: false,
     },
     {
         id: 2,
@@ -24,9 +24,9 @@ const dataSet = [
         courseCategory: "Skill Development",
         courseStatus: "Completed",
         lableColor: 'green',
-        dueDate: "15-APR-2022",
-        remainDays: 5,
-        action: "Print Certificate"
+        dueDate: "9-MAY-2022",
+        action: "Print Certificate",
+        mandatory: false,
     },
     {
         id: 3,
@@ -34,9 +34,9 @@ const dataSet = [
         courseCategory: "Orientation",
         courseStatus: "Pending",
         lableColor: 'blue',
-        dueDate: "18-FEB-2022",
-        remainDays: 30,
-        action: "Register"
+        dueDate: "18-JULY-2023",
+        action: "Register",
+        mandatory: true,
     },
     {
         id: 4,
@@ -44,24 +44,46 @@ const dataSet = [
         courseCategory: "Orientation",
         courseStatus: "Expired",
         lableColor: 'red',
-        dueDate: "18-DEC-2022",
-        remainDays: 30,
-        action: "Register"
+        dueDate: "18-MAY-2022",
+        action: "Register",
+        mandatory: false,
     }
 ]
 
 const initiateTableData = (element) => {
 
-    dataSet.map(({ courseTitle, courseCategory, courseStatus, dueDate, action, remainDays, lableColor }) => {
-        let date = new Date(dueDate)
+    dataSet.map(({ courseTitle, courseCategory, courseStatus, dueDate, action, lableColor, mandatory }) => {
+        let date = new Date(dueDate);
+        let today = new Date();
+        const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+        let remainDays = Math.round(Math.abs((date - today) / oneDay));;
         let sortingFactor = date.getFullYear().toString() + [date.getMonth() + 1].toString() + date.getDate().toString()
         const CourseData = `
-            <td class="coursetitle">${courseTitle}</td> 
+            <td>
+                <div class="d-flex flex-row justify-content-start align-items-center">
+                    <div class="myiconspan align-items-center position-relative ">
+                        <i class="bi bi-mortarboard myplanicon "></i>
+                        <i class="bi bi-exclamation-triangle-fill position-absolute warningicon ${mandatory ? 'd-block' : 'd-none'}"></i>
+                    </div>
+                    <div class="coursetitle">
+                        ${courseTitle}
+                    </div>
+                </div>
+            </td>
             <td class="text-nowrap course-category">${courseCategory}</td>
             <td class="text-nowrap course-status">
                 <p class="status-label status-label-${lableColor}">${courseStatus}</p>
             </td>
-            <td data-sort="${sortingFactor}" class="d-flex flex-column" ><p class="mb-0 remaindayscolum">${dueDate}</p><p class="mb-1 fst-italic remaindaystext">${remainDays} days more</p></td>
+            <td data-sort="${sortingFactor}" >
+                <div class="d-flex flex-column align-items-start"> 
+                    <div>
+                        <p class="mb-0 remaindayscolum">${dueDate}</p>
+                    </div>
+                    <div>
+                        <p class="mb-1 fst-italic ${remainDays > 5 ? 'remaindaystext' : 'endingremaindaystext'} ">${remainDays} days more</p>
+                    </div>
+                </div>
+            </td>
             <td class="text-nowrap actiontext position-relative"><div class="d-flex align-items-center justify-content-between" ><a href="#">${action}</a><button class="deletebtn">
                 <i class="bi bi-three-dots "></i>
 
